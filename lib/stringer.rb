f5bbc1c3aebcfd6ca9daca7aad9b9e95b1eeb7f1
Amenda <amenda@Catmendas-MacBook-Air.local>
2018-04-12 14:46:36 -0700

    Added minify, replacify, tokenize

--- lib/stringer.rb
+++ lib/stringer.rb
@@ -1,5 +1,46 @@
 require "stringer/version"
 
 module Stringer
-  # Your code goes here...
+    def self.spacify *strings
+        string = ""
+        strings.each do |s|
+            string += " " + s
+        end
+        return string
+    end
+
+
+    def self.minify(string, num)
+        if string.length > num
+            split = string.split(//)
+            newStr = ""
+            $i = 0
+            $numz = num - 1
+            while $i < $numz do
+                newStr+= split[$i]
+                $i += 1
+            end
+            return newStr + "..."
+        else
+            return string
+        end
+    end
+
+
+    def self.replacify(str, word, replacement)
+        split = str.split(" ")
+        for i in 0...split.length
+            if split[i] == word
+                split[i] = replacement
+            end
+        end
+        return split.join(" ")
+    end
+
+    def self.tokenize(str)
+        split = str.split(" ")
+        return split
+    end
+
+
 end
--- spec/stringer_spec.rb
+++ spec/stringer_spec.rb
@@ -1,9 +1,25 @@
+require "spec_helper"
 RSpec.describe Stringer do
-  it "has a version number" do
-    expect(Stringer::VERSION).not_to be nil
+
+  it "concatenates undefined number of strings with a space" do
+    expect(Stringer.spacify "Oscar", "Vazquez", "Zweig", "Olasaba", "Hernandez", "Ricardo", "Mendoza").to eq(" Oscar Vazquez Zweig Olasaba Hernandez Ricardo Mendoza")
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "shortens the length of the string to x-amount of characters followed by ..." do
+      expect(Stringer.minify("Hello my name is Amenda", 10)).to eq("Hello my ...")
   end
+
+  it "does not shorten the length of the string if it does not exceed maximum length" do
+      expect(Stringer.minify("Amenda", 10)).to eq("Amenda")
+  end
+
+  it "given a string, it replaces a specified word with a new one" do
+      expect(Stringer.replacify("I love puppies", "puppies", "kittens")).to eq('I love kittens')
+  end
+
+  it "iterates over a string and adds each word into an array" do
+      expect(Stringer.tokenize("I love kittens")).to eq(["I", "love", "kittens"])
+  end
+
+
 end
